03ed8aade5d91d9dbd2dc4695f8d56e642639c3e
Chao <hechaorun@qq.com>
2017-03-19 10:45:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!--<div id="app">
+    <img src="./assets/index.png">
+    <hello></hello>
+  </div>-->
+  <lie></lie>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Lie from './components/Lie'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Lie
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family:"microsoft yahei";
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lie.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="page">
+    <!--page1-->
+    <div v-if="page===1" class="page1">
+      <img class="full-width-img" src="../assets/index.png">
+      <div class="face-container">
+        <img class="face" src="../assets/index_face.gif">
+      </div>
+      <div class="bottom">
+        <h1>让朋友更了解你的态度</h1>
+        <img class="btn" @click="page=2" src="../assets/set_title_btn.png">
+      </div>
+    </div>
+    <!--page2-->
+    <div v-if="page===2" class="page2">
+      <img class="full-width-img" src="../assets/edit_top.png">
+      <div class="full-width-img title_bg"></div>
+      <img class="full-width-img title_bg" src="../assets/title_bg.png">
+      <div class="container">
+        <div style="text-align: center;">
+          <span style="color:#b46e53;">只选择你的真心话</span>
+        </div>
+        <div class="head">
+          <div class="title">&nbsp;{{ question.qData[0].txt }}</div>
+          <div class="change" @click="changeQuestion">换一张</div>
+          <div class="clearfix"></div>
+        </div>
+        <div class="answer" v-bind:class="[select==0 ? 'check' : 'not-check']" @click="select=0">{{ question.as[0] }}</div>
+        <div class="answer" v-bind:class="[select==1 ? 'check' : 'not-check']" @click="select=1">{{ question.as[1] }}</div>
+        <img v-if="select == -1" class="btn next-btn" @click="page=2" src="../assets/next_disabled.png">
+        <img v-else class="next-btn" @click="nextQuestion" src="../assets/next.png">
+        <div class="container-bottom">
+          <span>第{{ questions.length + 1 }}题/共5题</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from '../data'
+
+export default {
+  name: 'lie',
+  data () {
+    return {
+      page: 1,
+      data: data,
+      questionIndex: 0,
+      select: -1,
+      questions: []
+    }
+  },
+  created () {
+    console.log(data.length)
+    this.changeQuestion()
+  },
+  methods: {
+    changeQuestion () {
+      let time = 0
+      let isInQuestion = 0
+      do {
+        time++
+        this.questionIndex = Math.floor(Math.random() * this.data.length)
+        console.log(this.questionIndex)
+        // 判断该道题是否已经做过
+        isInQuestion = this.questions.filter((question) => {
+          return question.index === this.questionIndex
+        }).length
+        console.log('isInQuestion:' + isInQuestion)
+      } while (isInQuestion && time < 10)
+      this.select = -1
+    },
+    nextQuestion () {
+      console.log('next')
+      this.questions.push({
+        index: this.questionIndex,
+        answer: this.select
+      })
+      this.changeQuestion()
+    }
+  },
+  computed: {
+    question () {
+      return this.data[this.questionIndex]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.page {
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
+  background-color: rgb(88, 95, 184);
+  position: relative;
+}
+.full-width-img {
+  width: 100%;
+}
+.face-container {
+  position: fixed;
+  left: 24.3%;
+  /*top: 26%;;*/
+  top: 4.6rem;
+  width: 2.3rem;
+}
+.face {
+  width: 100%;
+}
+.page1 h1 {
+  color: #fff;
+  font-size: .5rem;
+  text-align: center;
+  font-weight: normal;
+}
+.bottom {
+  width: 100%;
+  margin-top: .2rem;
+}
+.page .btn {
+  width: 60%;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+.page2 {
+  padding: .2em .5rem;
+  position: relative;
+}
+.title_bg {
+  margin-top: 1.4rem;
+}
+.container {
+  /*background-color: rgba(216, 194, 194, 0.65);*/
+  width: 85%;
+  height: 55%;
+  position: fixed;
+  top: 4rem;
+  padding: .2rem;
+}
+.container span {
+  font-size: .5rem;
+}
+.container .head {
+  width: 100%;
+  margin-bottom: .1rem;
+  /*padding: 1em 0;*/
+}
+.head .title {
+  width: 75%;
+  color: rgb(33, 55, 127);
+  font-size: .7rem;
+  /*background-color: #f00;*/
+  float: left;
+}
+.head .change {
+  width: 25%;
+  color: rgba(33, 55, 127, 0);
+  font-size: .6rem;  
+  /*background-color: #0f0;*/
+  float: left;
+}
+.clearfix {
+  clear: both;
+}
+.container .answer {
+  background: #ffd766;
+  padding: .2rem;
+  font-size: .5rem;
+  color: rgb(33, 55, 127);
+  margin-bottom: .3rem;
+  border-radius: .2rem;
+}
+.container .not-check::before {
+  content: '';
+  background: url(../assets/check.png) left bottom no-repeat;
+  background-size: 100% auto;
+  width: .8rem;
+  height: .8rem;
+  display: inline-block;
+  margin-right: .1rem;
+}
+.container .check::before {
+  content: '';
+  background: url(../assets/check.png) left top no-repeat;
+  background-size: 100% auto;
+  width: .8rem;
+  height: .8rem;
+  display: inline-block;
+  margin-right: .1rem;
+}
+.next-btn {
+  width: 45%;
+  position: absolute;
+  /*bottom: .3rem;*/
+  top: 7.2rem;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+.container-bottom {
+  color: #b46e53;
+  font-size: .8rem;
+  position: absolute;
+  /*bottom: .1rem;*/
+  top: 9rem;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+</style>
